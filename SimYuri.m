45ad4e93234d1f9fb941090fabe44ea253d70018
Arality <arality@gmail.com>
2020-04-03 14:48:30 -0700

    updaing based on classwork

--- SimYuri.m
+++ SimYuri.m
@@ -22,36 +22,28 @@ global key;
 InitKeyboard();
 global event;
 running = true;
-event = EVENTS.MANUAL;
+%event = EVENTS.MANUAL;
+event = EVENTS.STARTUP;
+global BUTTON;
+BUTTON = brick.TouchPressed(BUTTONSENSORPORT);
+global AUTONOMOUSMODE;
+AUTONOMOUSMODE = true;
 
 while running
-    %if brick.TouchPressed(1)
-    %    reverse12(brick, LEFTMOTORPORT, RIGHTMOTORPORT);
-    %end
-    event = EVENTS.MANUAL;
-    switch event
-        case EVENTS.STARTUP
-            event = EVENTS.MANUAL;
-        case EVENTS.FORWARD
-            moveForward24(bot, port, speed);
-        case EVENTS.REVERSE
-            
-        case EVENTS.STOPPED
-            wallDistance = getDistance(brick, ULTRASONICSENSORPORT);
-            if wallDistance <= 24
-                % There's a wall here
-            elseif wallDistance  > 24
-                % No Wall
-            end
-        case EVENTS.TURNING
-            
-        case EVENTS.MANUAL
+    pause(0.1);
+    switch BUTTON
+    case 1  % Button Pressed
+        reverse12(brick, LEFTMOTORPORT, RIGHTMOTORPORT);
+        event = EVENTS.STOPPED
+    case 0  % Button not pressed
+        switch AUTONOMOUSMODE
+        case false  % Yuri under manual control
             manualControl = true;
             while manualControl
                 pause(0.1);
                 switch key
                     case 'uparrow'
-                        moveForward(brick, BOTHMOTORSPORT);
+                        continousMoveForward(brick, BOTHMOTORSPORT);
                     case 'downarrow'
                         brick.StopAllMotors();
                     case 'rightarrow'
@@ -64,16 +56,50 @@ while running
                         manualControl = false;
                         event = EVENTS.QUIT;
                         break;
+                    case 'c'
+                        manualControl = false;
+                        event = EVENTS.STOPPED;
+                        break;
                 end
             end
-        case EVENTS.PICKUP
-            
-        case EVENTS.DROPOFF
-            
-        case EVENTS.QUIT
-            CloseKeyboard();
-            running = false;
-    end
+        case true   % Yuri under autonomous control
+            switch event
+            case EVENTS.STARTUP
+                % Setup sim values
+                
+                event = EVENTS.STOPPED;
+            case EVENTS.FORWARD
+                for i = 0:3
+                    touch = brick.TouchPressed(BUTTONSENSORPORT);
+                    if touch
+                        event = EVENTS.REVERSE;
+                    end
+                    moveForward(brick, LEFTMOTORPORT, RIGHTMOTORPORT, 6)
+                    i++;
+                event = EVENTS.STOPPED;
+            case EVENTS.REVERSE
+                reverse12(brick, LEFTMOTORPORT, RIGHTMOTORPORT);
+                event = EVENTS.STOPPED;
+            case EVENTS.STOPPED
+                wallDistance = getDistance(brick, ULTRASONICSENSORPORT);
+                if wallDistance <= 24
+                    event = EVENTS.FORWARD;
+                elseif wallDistance  > 24
+                    event = EVENTS.TURNING;
+                end
+            case EVENTS.TURNING
+                turnRight90(brick, LEFTMOTORPORT, RIGHTMOTORPORT);
+                event = EVENTS.STOPPED;
+            case EVENTS.MANUAL
+    
+            case EVENTS.PICKUP
+                
+            case EVENTS.DROPOFF
+                
+            case EVENTS.QUIT
+                CloseKeyboard();
+                running = false;
+        end
 end
 
 % Ultrasonic code
@@ -87,16 +113,21 @@ function convertedNumber = convertToInches(number)
 convertedNumber = number * 0.3937;
 end
 
+% Converts REL angle to distance in inches
+function angle = convertREL(inches)
+angle = inches * 122.9167;  % 122.9167 = (2950/24) = (moveMotorAngleRel distance measured over 24" \ 24")
+end
+
 % Move Motor forward 24 inches
-function moveForward24 = moveForward24(bot, port)
-bot.MoveMotor(port, 100);
-%Seconds is calculated using distance (24") over speed (9inches/second)
-pause(2.666);
-bot.StopMotor(port);
+function moveForward = moveForward(bot, leftMotor, rightMotor, inchDistance)
+% 2950 = 24"
+% 1475 = 12"
+brick.MoveMotorAngleRel(leftMotor, 100, convertREL(inchDistance), 'Coast')
+brick.MoveMotorAngleRel(rightMotor, 100, convertREL(inchDistance), 'Coast')
 end
 
 % Keep moving forward
-function moveForward = moveForward(bot, port)
+function continousMoveForward = continousMoveForward(bot, port)
 bot.MoveMotor(port, 100);
 end
 
@@ -141,7 +172,7 @@ end
 %Move Motor backwards 12 inches
 function reverse12 = reverse12(bot, leftMotor, rightMotor)
 bot.MoveMotorAngleRel(leftMotor, 70, 1000, 'Coast');
-bot.MoveMotorAngleRel(rightrMotor, 70, -1000, 'Coast');
+bot.MoveMotorAngleRel(rightMotor, 70, -1000, 'Coast');
 bot.StopAllMotors;
 end
 
@@ -152,6 +183,7 @@ end
 
 %{
 Functions to be implemented:
+
 
 
 %}
